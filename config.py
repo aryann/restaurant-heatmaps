520f17d30c04e6bd42123ef1e4c8822dd78bc8b3
Aryan Naraghi <aryan.naraghi@gmail.com>
2015-12-14 21:47:06 -0800

    Adds search functionality.

--- config.py
+++ config.py
@@ -3,11 +3,14 @@ import os
 import jinja2
 
 
+DEFAULT_LAT_LON = (47.608013, -122.335167)  # Seattle, WA
+
 DEBUG = True
 
 PLACES_API_KEY = 'AIzaSyBKqLZqJwhRMAOnyogfkFDetkA0iEXtDbk'
 
 SEARCH_INDEX_NAME = 'places'
+MAX_SEARCH_RESULTS = 1000
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import json
 import webapp2
+
+from google.appengine.api import search
 
 import config
 
@@ -6,8 +9,54 @@ import config
 class MainPageHandler(webapp2.RequestHandler):
 
     def get(self):
+        # Grab the lat and lon query parameters if they both exist and
+        # if they can both be parsed as floats.
+        lat, lon = self.request.get('latitude'), self.request.get('longitude')
+        try:
+            lat = float(lat)
+            lon = float(lon)
+        except ValueError:
+            lat, lon = None, None
+
+        # We could get the latitude and longitude from the query
+        # params, so fall back to default location.
+        if not lat or not lon:
+            lat, lon = config.DEFAULT_LAT_LON
+
+        # Perform the search for the given location.
+        index = search.Index(name=config.SEARCH_INDEX_NAME)
+        results = index.search(
+            query=search.Query(
+                query_string=(
+                    'distance(location, geopoint({lat}, {lon})) < 10000'.format(
+                        lat=lat, lon=lon)),
+                options=search.QueryOptions(
+                    returned_fields=[
+                        'location',
+                    ],
+                    limit=config.MAX_SEARCH_RESULTS)))
+
+        # Convert the search results to a JSON-serializable list that
+        # can be passed on to the JavaScript code in the template.
+        places = []
+        for res in results:
+            value = res.fields[0].value
+            places.append((value.latitude, value.longitude))
+
+        if config.DEBUG:
+            debug = json.dumps({
+                'num_found': results.number_found,
+            })
+        else:
+            debug = ''
+
         template = config.JINJA_ENVIRONMENT.get_template('main.html')
-        self.response.write(template.render({}))
+        self.response.write(template.render({
+            'debug': debug,
+            'lat': lat,
+            'lon': lon,
+            'places': json.dumps(places),
+        }))
 
 
 handlers = webapp2.WSGIApplication([
