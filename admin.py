5b27a848c260787c49fd48e57b72b07514899714
Aryan Naraghi <aryan.naraghi@gmail.com>
2015-12-20 19:16:39 -0800

    Converted the code that adds places to use Task Queues.

--- admin.py
+++ admin.py
@@ -8,59 +8,21 @@ import webapp2
 
 from google.appengine.ext import ndb
 from google.appengine.api import search
+from google.appengine.api import taskqueue
 
 import config
 import models
 
 R_EARTH = 6378137.0
 
-def get_all_places(lat, lon, radius):
-    logging.info('Fetching results for lat=%f, lon=%f', lat, lon)
-    location = '{0},{1}'.format(lat, lon)
-    url = ('https://maps.googleapis.com/maps/api/place/radarsearch/'
-           'json?key={key}&radius={radius}&location={location}&'
-           'type=restaurant').format(
-               key=config.MAPS_API_KEY,
-               radius=radius,
-               location=location)
-    res = json.load(urllib2.urlopen(url))
-    logging.info('Num results from %s: %d', url, len(res['results']))
-
-    places = []
-
-    # If number of results >= 200, then that means we hit the Radar
-    # Search's result limit, so we continue by dividing the geographic
-    # search location into four smaller areas, and sending separate
-    # requests for each area.
-    if len(res['results']) >= 200:
-        logging.info('Dividing request to four smaller ones.')
-
-        dx = radius / 2.0 * math.cos(math.radians(45))
-        dy = radius / 2.0 * math.sin(math.radians(45))
-
-        for dir_x in (-1, 1):
-            for dir_y in (-1, 1):
-                new_lat = lat + dir_y * (dy / R_EARTH * 180 / math.pi)
-                new_lon = lon + dir_x * (dx / R_EARTH * 180 / math.pi /
-                                           math.cos(lat * math.pi / 180))
-
-                time.sleep(1)  # Keep requests to a reasonable rate.
-                places.extend(get_all_places(new_lat, new_lon, radius / 2.0))
-
-    # If number of results < 200, we did not hit any limits, so there
-    # is no need to divide the area into smaller areas.
-    else:
-        for place in res['results']:
-            places.append(place)
-
-    return places
-
 
 class AddCityHandler(webapp2.RequestHandler):
 
     def get(self):
         template = config.JINJA_ENVIRONMENT.get_template('add_city.html')
-        self.response.write(template.render())
+        self.response.write(template.render({
+            'debug': config.DEBUG,
+        }))
 
     def post(self):
         woeid = self.request.POST['woeid']
@@ -75,25 +37,73 @@ class AddCityHandler(webapp2.RequestHandler):
             ready=False)
         city.put()
 
-        # TODO: This will not work in practice because App Engine
-        # limits how long a request can take. Fix this by using Task
-        # Queues for fetching places.
-        places = get_all_places(lat, lon, 15000)
+        taskqueue.add(url='/admin/addcity/worker', params={
+            'lat': lat,
+            'lon': lon,
+            'radius': 15000,
+        })
 
-        index = search.Index(name=config.SEARCH_INDEX_NAME)
-        for place in places:
-            location = place['geometry']['location']
-            doc = search.Document(
-                doc_id=place['place_id'],
-                fields=[
-                    search.GeoField(name='location',
-                                    value=search.GeoPoint(
-                                        latitude=location['lat'],
-                                        longitude=location['lng'])),
-            ])
-            index.put(doc)
+        self.redirect('/')
+
+
+class AddCityWorker(webapp2.RequestHandler):
+
+    def post(self):
+        lat = float(self.request.get('lat'))
+        lon = float(self.request.get('lon'))
+        radius = float(self.request.get('radius'))
+
+        location = '{0},{1}'.format(lat, lon)
+        url = ('https://maps.googleapis.com/maps/api/place/radarsearch/'
+               'json?key={key}&radius={radius}&location={location}&'
+               'type=restaurant').format(
+                   key=config.MAPS_API_KEY,
+                   radius=radius,
+                   location=location)
+        res = json.load(urllib2.urlopen(url))
+        logging.info('Num results for lat=%f, lon=%f, radius=%f: %d',
+                     lat, lon, radius, len(res['results']))
+
+        # If number of results >= 200, then that means we hit the Radar
+        # Search's result limit, so we continue by dividing the geographic
+        # search location into four smaller areas, and sending separate
+        # requests for each area.
+        if len(res['results']) >= 200:
+            logging.info('Dividing request to four smaller ones.')
+
+            dx = radius / 2.0 * math.cos(math.radians(45))
+            dy = radius / 2.0 * math.sin(math.radians(45))
+
+            for dir_x in (-1, 1):
+                for dir_y in (-1, 1):
+                    new_lat = lat + dir_y * (dy / R_EARTH * 180 / math.pi)
+                    new_lon = lon + dir_x * (dx / R_EARTH * 180 / math.pi /
+                                             math.cos(lat * math.pi / 180))
+
+                    taskqueue.add(url='/admin/addcity/worker', params={
+                        'lat': new_lat,
+                        'lon': new_lon,
+                        'radius': radius / 2.0,
+                    })
+
+        # If number of results < 200, we did not hit any limits, so there
+        # is no need to divide the area into smaller areas.
+        else:
+            index = search.Index(name=config.SEARCH_INDEX_NAME)
+            for place in res['results']:
+                location = place['geometry']['location']
+                doc = search.Document(
+                    doc_id=place['place_id'],
+                    fields=[
+                        search.GeoField(name='location',
+                                        value=search.GeoPoint(
+                                            latitude=location['lat'],
+                                            longitude=location['lng'])),
+                ])
+                index.put(doc)
 
 
 handlers = webapp2.WSGIApplication([
     ('/admin/addcity', AddCityHandler),
+    ('/admin/addcity/worker', AddCityWorker),
 ], debug=config.DEBUG)
